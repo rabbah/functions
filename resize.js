a6adcce54c9df0f8420d3edfb6ac042cbb878fa9
Rodric Rabbah <rodric@gmail.com>
2019-02-07 14:23:00 -0500

    Make it work with existing OpenWhisk nodejs actions and add test logic.

--- resize.js
+++ resize.js
@@ -14,7 +14,7 @@
 
 'use strict'
 
-const gm = require('gm')
+const gm = require('gm').subClass({imageMagick: true});
 const fs = require('fs')
 
 function resize(fileSrc, fileDst, w, h) {
@@ -56,4 +56,13 @@ function main({img, w, h}) {
     console.error(error)
     return error
   })
+}
+
+if (process.env.TEST) {
+  let i = "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGCAYAAAD68A/GAAAA/klEQVQYGWNgAAEHBxaG//+ZQMyyn581Pfas+cRQnf1LfF" +
+          "Ljf+62smUgcUbt0FA2Zh7drf/ffMy9vLn3RurrW9e5hCU11i2azfD4zu1/DHz8TAy/foUxsXBrFzHzC7r8+M9S1vn1qxQT07" +
+          "dDjL9fdemrqKxlYGT6z8AIMo6hgeUfA0PUvy9fGFh5GWK3z7vNxSWt++jX99+8SoyiGQwsW38w8PJEM7x5v5SJ8f+/xv8MDA" +
+          "zffv9hevfkWjiXBGMpMx+j2awovjcMjFztDO8+7GF49LkbZDCDeXLTWnZO7qDfn1/+5jbw/8pjYWS4wZLztXnuEuYTk2M+Mz" +
+          "Iw/AcA36VewaD6fzsAAAAASUVORK5CYII="
+  main({img: i, w: 128, h: 128}).then(console.log)
 }
