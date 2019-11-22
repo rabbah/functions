883139c307b892ca454653a6e11f93e833ad1853
rodric rabbah <rodric@gmail.com>
2019-11-22 05:13:23 -0800

    Update resize-url.js

--- resize-url.js
+++ resize-url.js
@@ -1,17 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict'
 
 const request = require('needle')
@@ -21,8 +7,8 @@ const fs = require('fs')
 // resize images
 function main(args) {
   let url     = args.url
-  let w       = args.w || 128
-  let h       = args.h || 128
+  let w       = parseInt(args.w || 128)
+  let h       = parseInt(args.h || 128)
   let fileSrc = '/tmp/image-src.dat'
   let fileDst = '/tmp/image-dst.dat'
 
